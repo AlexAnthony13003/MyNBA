f2d2a550c6c8da1c1397e4ea345da43c64462981
AlexAnthony13003 <alexisguillermain.pro@gmail.com>
2023-08-14 14:30:37 +0200

    🗑️ MyTeam

--- src/Service/CallApiService.php
+++ src/Service/CallApiService.php
@@ -24,6 +24,20 @@ class CallApiService
 
         return json_decode($response->getBody(), true);
     }
+
+    public function getSpecificTeam(int $id): array
+    {
+        $url = 'https://free-nba.p.rapidapi.com/teams/' . $id;
+
+        $response = $this->client->request('GET', $url, [
+            'headers' => [
+                'X-RapidAPI-Host' => 'free-nba.p.rapidapi.com',
+                'X-RapidAPI-Key' => '1c091c85d4msh6a0456eaccc958ap1f80c2jsne7fc2f278e62',
+            ],
+        ]);
+        return json_decode($response->getBody(), true);
+    }
+
     public function getMatches(): array
     {
         $response = $this->client->request('GET', 'https://free-nba.p.rapidapi.com/games?page=0', [
